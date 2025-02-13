00439235db768f54a6747d7c784a3fc3fe0a24f1
sigggy <jackbsigler@gmail.com>
2025-02-12 19:27:15 -0700

    changing back some things in main for the pull request

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,10 @@
 
 #define DEBUG_MAIN	DEBUG || 0
 
-#define DEFAULT_PRINT_ACTIONS	true
-#define DEFAULT_PRINT_RESULTS	1
+#define DEFAULT_PRINT_ACTIONS	false
+#define DEFAULT_PRINT_RESULTS	0
 #define DATA_LOG_FORMAT	"alg_%d.dat"
-#define DEFAULT_DATA_LOG_PATH	"./"
+#define DEFAULT_DATA_LOG_PATH	"0"
 #define DEFAULT_RUN_NUM	0
 #define DEFAULT_VEHICLE_DEFINE_LOCATION "../../VehicleInputs/StandardDefinitionVehicle.yaml"
 
